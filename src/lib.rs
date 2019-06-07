6e1dccec224e06a9f71a0faccbd47bf453c3ea63
Andrew Jones <ascjones@gmail.com>
2019-06-07 16:05:35 +0100

    Use compatible local types instead
    Using node-runtime types directly was costly in terms of code size of
    contracts. This just mirrors the types, and includes tests which ensures
    the codec implementation roundtrip succeeds

--- src/lib.rs
+++ src/lib.rs
@@ -19,15 +19,109 @@
 
 #![cfg_attr(not(any(test, feature = "test-env")), no_std)]
 
-use srml_contract;
-use node_runtime;
+use core::{
+    array::TryFromSliceError,
+    convert::TryFrom,
+};
+
+use parity_codec::{Encode, Decode};
 
 /// Contract environment types defined in substrate node-runtime
 pub enum NodeRuntimeTypes {}
 
+/// The default SRML address type.
+#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash, Encode, Decode)]
+pub struct AccountId([u8; 32]);
+
+impl From<[u8; 32]> for AccountId {
+    fn from(address: [u8; 32]) -> AccountId {
+        AccountId(address)
+    }
+}
+
+impl<'a> TryFrom<&'a [u8]> for AccountId {
+    type Error = TryFromSliceError;
+
+    fn try_from(bytes: &'a [u8]) -> Result<AccountId, TryFromSliceError> {
+        let address = <[u8; 32]>::try_from(bytes)?;
+        Ok(AccountId(address))
+    }
+}
+
+/// The default SRML balance type.
+pub type Balance = u128;
+
+/// The default SRML hash type.
+#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash, Encode, Decode)]
+pub struct Hash([u8; 32]);
+
+impl From<[u8; 32]> for Hash {
+    fn from(hash: [u8; 32]) -> Hash {
+        Hash(hash)
+    }
+}
+
+impl<'a> TryFrom<&'a [u8]> for Hash {
+    type Error = TryFromSliceError;
+
+    fn try_from(bytes: &'a [u8]) -> Result<Hash, TryFromSliceError> {
+        let hash = <[u8; 32]>::try_from(bytes)?;
+        Ok(Hash(hash))
+    }
+}
+
+/// The default SRML moment type.
+pub type Moment = u64;
+
 impl ink_core::env::EnvTypes for NodeRuntimeTypes {
-    type AccountId = srml_contract::AccountIdOf<node_runtime::Runtime>;
-    type Balance = srml_contract::BalanceOf<node_runtime::Runtime>;
-    type Hash = srml_contract::SeedOf<node_runtime::Runtime>;
-    type Moment = srml_contract::MomentOf<node_runtime::Runtime>;
+    type AccountId = AccountId;
+    type Balance = Balance;
+    type Hash = Hash;
+    type Moment = Moment;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use srml_contract::AccountIdOf;
+    use node_runtime::Runtime;
+    use parity_codec::{Encode, Decode};
+    use quickcheck_macros::quickcheck;
+
+    #[derive(Debug, Clone)]
+    struct ContractAccountId(AccountId);
+
+    impl quickcheck::Arbitrary for ContractAccountId {
+        fn arbitrary<G: quickcheck::Gen>(g: &mut G) -> Self {
+            let mut res = [0u8; core::mem::size_of::<Self>()];
+			g.fill_bytes(&mut res[..]);
+            ContractAccountId(AccountId(res)) 
+        }
+    }
+
+    #[quickcheck]
+    fn account_id(value: ContractAccountId) {
+        let contract_encoded = Encode::encode(&value.0);
+        let runtime_decoded: AccountIdOf<Runtime> = Decode::decode(&mut contract_encoded.as_slice())
+            .expect("Should be decodable into node_runtime type");
+        let runtime_encoded = Encode::encode(&runtime_decoded);
+        let contract_decoded = Decode::decode(&mut runtime_encoded.as_slice())
+            .expect("Should be decodable into contract env type");
+        assert_eq!(value.0, contract_decoded)
+    }
+
+    // #[test]
+    // pub fn balance() {
+    //     let x: <NodeRuntimeTypes as ink_core::env::EnvTypes>::Balance = ();
+    // }
+
+    // #[test]
+    // pub fn hash() {
+    //     let x: <NodeRuntimeTypes as ink_core::env::EnvTypes>::Hash = ();
+    // }
+
+    // #[test]
+    // pub fn moment() {
+    //     let x: <NodeRuntimeTypes as ink_core::env::EnvTypes>::Moment = ();
+    // }
 }
