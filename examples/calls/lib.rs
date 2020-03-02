b7cdf83d56a00605218f4e42691602a32ae1f5b6
Andrew Jones <ascjones@gmail.com>
2020-03-02 16:23:12 +0000

    print result of balance transfer

--- examples/calls/lib.rs
+++ examples/calls/lib.rs
@@ -5,6 +5,8 @@ use ink_lang as ink;
 
 #[ink::contract(version = "0.1.0", env = NodeRuntimeTypes)]
 mod calls {
+    use ink_core::env;
+    use ink_prelude::*;
     use ink_types_node_runtime::{calls as runtime_calls, NodeRuntimeTypes};
 
     /// This simple dummy contract dispatches substrate runtime calls
@@ -18,8 +20,14 @@ mod calls {
         /// Dispatches a `transfer` call to the Balances srml module
         #[ink(message)]
         fn balance_transfer(&self, dest: AccountId, value: Balance) {
+            // create the Balances::transfer Call
             let transfer_call = runtime_calls::transfer_balance(dest, value);
-            let _ = self.env().invoke_runtime(&transfer_call);
+            // dispatch the call to the runtime
+            let result = self.env().invoke_runtime(&transfer_call);
+
+            // report result to console
+            // NOTE: println should only be used on a development chain)
+            env::println(&format!("Balance transfer invoke_runtime result {:?}", result));
         }
     }
 
